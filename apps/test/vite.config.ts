198af759f70b857c8be0e2ad039ec125a0670994
MengxiQ <xw_wei_lx@gxbolian.com.cn>
2024-07-01 17:46:46 +0800

    测试单页应用程序

---
+++ apps/test/vite.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/apps/app-test/',
+  build: {
+    outDir: '../app-test'
+  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
