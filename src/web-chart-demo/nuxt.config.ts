290321d75bc276982198b657bf1b451b4347bab8
MengxiQ <xw_wei_lx@gxbolian.com.cn>
2024-07-02 16:10:40 +0800

    _payload.json 404的问题

--- src/web-chart-demo/nuxt.config.ts
+++ src/web-chart-demo/nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  experimental: {
+    //_payload.json 404的问题
+    payloadExtraction: false,
+  },
   app: {
     baseURL: '/apps/web-chart-demo/',
     // 默认的_nuxt目录会被服务器拦截
